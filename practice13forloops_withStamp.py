34559ed051784861deaa63e0426bdf6ac561fc7e
Asuncion Mendoza <asuncionmendoza@MacBook-Air.local>
2023-08-29 11:43:59 -0700

    New

--- practice13forloops_withStamp.py
+++ practice13forloops_withStamp.py
@@ -1,11 +1,11 @@
 import turtle as trtl 
 painter = trtl.Turtle()
 painter.color("blue")
-painter.shape("circle")
 painter.fillcolor("red")
 
 for move in range(5):
     painter.circle(20)
+    painter.fillcolor("blue")
     painter.penup()
     painter.forward(50)
     painter.right(75)
