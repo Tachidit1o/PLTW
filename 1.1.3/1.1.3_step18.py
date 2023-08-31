de5e224e59672c6a876bc8a8861441177e24788b
Asuncion Mendoza <asuncionmendoza@MacBook-Air.local>
2023-08-31 15:11:42 -0700

    New

--- 1.1.3/1.1.3_step18.py
+++ 1.1.3/1.1.3_step18.py
@@ -12,17 +12,23 @@ num_of_floors = 63
 
 for little_guy in range(num_of_floors):
     painter.penup()
+
+    axe = little_guy % 63
+    if axe > 42: 
+        painter.pencolor("red")
+        x = 100
+    
+    elif axe >  21:
+        painter.penup()
+        painter.pencolor("blue")
+        x = 0
+        y = -150
+
     painter.goto(x,y)
     painter.pendown()
     painter.forward(50)
-    
-    axe = little_guy % 63
-    if axe >= 42  : 
-        painter.pencolor("red")
-    elif axe >=  21:
-        painter.pencolor("blue")
 
-    y += 5
+    y = y + 5
 
 
 wn = trtl.Screen()
---
+++ 1.1.3/1.1.3_step19.py
@@ -0,0 +1,29 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+x = -150
+y = -200
+
+floors = 63
+
+    painter.penup()
+    painter.goto(x,y)
+    painter.pendown()
+    painter.forward(50)
+
+for me in range(floors):
+
+
+
+    remainder = me % 63
+    if remainder >= 42:
+        painter.pencolor("green")
+    elif remainder >= 21: 
+        painter.pencolor("red")
+
+    y += 5
+
+
+wn = trtl.Screen()
+wn.mainloop()
