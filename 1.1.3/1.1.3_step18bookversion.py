6175ed8b714c95c270cde9cff2608ea8b4aa6b8a
Asuncion Mendoza <asuncionmendoza@macbook-air.mynetworksettings.com>
2023-09-03 21:24:48 -0700

    New

--- 1.1.3/1.1.3_step18bookversion.py
+++ 1.1.3/1.1.3_step18bookversion.py
@@ -12,12 +12,13 @@ for runner in range(num_of_floors):
     painter.penup()
 
     remainder = runner % 6 
+#teach the students that the remainder greater than or equal to 4 will be blue
     if remainder >= 4: 
         painter.pencolor("blue")
 
     elif remainder >= 2:
         painter.pencolor("red")
-
+#Teach scholars that the below is how you start a new tower
     remainder = runner % 21
     if remainder == 0: 
         y = -150
