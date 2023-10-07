e0f34e62dd743236af3739eb40b6cdabadb36edd
Asuncion Mendoza <asuncionmendoza@macbook-air.mynetworksettings.com>
2023-10-06 23:26:01 -0700

    Activity 1.1.1

--- 1.1.1/1.1.1.py
+++ 1.1.1/1.1.1.py
@@ -1,11 +1,20 @@
-1.1.1.py
-
 import turtle as trtl
 
 painter = trtl.Turtle()
-
+painter.turtlesize(9)
+painter.pensize(3)
 painter.forward(100)
-painter.right(90)
- 
+painter.right(40)
+
+methodes_and_attributes = dir(painter)
+"""
+for index, item in enumerate(methodes_and_attributes):
+    print(f"Index {index}: {item}")
+"""
+
+for item in enumerate(methodes_and_attributes):
+    print(item)
+
 wn = trtl.Screen()
 wn.mainloop()
+
