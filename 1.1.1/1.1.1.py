4b8e4d751e6392c979d1584ef30d5dab98f470b7
Asuncion Mendoza <asuncionmendoza@MacBook-Air.lan>
2023-11-04 15:12:11 -0700

    Now

--- 1.1.1/1.1.1.py
+++ 1.1.1/1.1.1.py
@@ -3,17 +3,24 @@ import turtle as trtl
 painter = trtl.Turtle()
 painter.turtlesize(9)
 painter.pensize(3)
+painter.left(90)
+
 painter.forward(100)
-painter.right(40)
+painter.right(90)
+painter.forward(100)
 
 methodes_and_attributes = dir(painter)
 """
 for index, item in enumerate(methodes_and_attributes):
     print(f"Index {index}: {item}")
-"""
+
+Note: The above code is different than this: 
 
 for item in enumerate(methodes_and_attributes):
     print(item)
+"""
+
+
 
 wn = trtl.Screen()
 wn.mainloop()
