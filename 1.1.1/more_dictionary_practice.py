7883de67a910301ffd77aa48e505422b4b7f246a
Asuncion Mendoza <asuncionmendoza@macbook-air.mynetworksettings.com>
2023-11-05 21:29:32 -0800

    New

--- 1.1.1/more_dictionary_practice.py
+++ 1.1.1/more_dictionary_practice.py
@@ -12,13 +12,20 @@ if (user_input == 'yes'):
 
     second_question = input("Is there an animal type you would like to delete (yes or no)?")
     if (second_question == "yes"):
-        third_question =  input("What type of animal category would you like to delete (please enter the category name only, e.g. 'air animal')?")
-        if (third_question in whatever):
-            del whatever[third_question]
-            print( f" {third_question} has been deleted. ")
+        while True: 
+#Enter a while loop to avoid anything else that is not in the dictionary. 
+            third_question =  input("What type of animal category would you like to delete (please enter the category name only, e.g. 'air animal')?")         
+            if (third_question in whatever):
+                del whatever[third_question]
+                print( f" {third_question} has been deleted. ")
 
-            for x, y in whatever.items():
-                print (x, y)
+                for x, y in whatever.items():
+                    print (x, y)    
+                break        
+            else: 
+                print('You did not enter an existing category')
+                        
+
 else: 
     print('Great, have a good day then...')
 
