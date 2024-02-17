d7ac820a3d1627374d47c0e89c833e06641373bb
Asuncion Mendoza <asuncionmendoza@macbook-air-2.mynetworksettings.com>
2024-02-16 20:44:41 -0800

    While loop

---
+++ 10thgrade/practice_while_loop.py
@@ -0,0 +1,5 @@
+answer = input('Hello and welcome to my program. Press y to enter the loop: ')
+while answer == 'y':
+    answer = input(' Press y to stay in the loop or n to exit: ')
+
+print ('You are out the loop.')
